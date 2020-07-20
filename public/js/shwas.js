7b0e842c1493ffd036e26f9e5de19143f476c4c3
ankur-mitt <54750191+ankur-mitt@users.noreply.github.com>
2020-07-21 01:04:05 +0530

    Add files via upload

--- public/js/shwas.js
+++ public/js/shwas.js
@@ -1,39 +1,137 @@
 console.log('shwas.js');
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+Chart.defaults.global.defaultFontSize = 15;
+
+var delhiPollutionLevels = new Chart(
+    document.getElementById('delhi-pollution-levels').getContext('2d'), {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: 'Delhi Pollution Levels in November 2019',
+                fill: true,
+                data: [{
+                    x: new Date('2019-11-01'),
+                    y: 380
+                }, {
+                    x: new Date('2019-11-02'),
+                    y: 330
+                }, {
+                    x: new Date('2019-11-03'),
+                    y: 550
+                }, {
+                    x: new Date('2019-11-04'),
+                    y: 260
+                }, {
+                    x: new Date('2019-11-06'),
+                    y: 100
+                }, {
+                    x: new Date('2019-11-07'),
+                    y: 190
+                }, {
+                    x: new Date('2019-11-08'),
+                    y: 150
+                }, {
+                    x: new Date('2019-11-09'),
+                    y: 140
+                }, {
+                    x: new Date('2019-11-11'),
+                    y: 254
+                }, {
+                    x: new Date('2019-11-12'),
+                    y: 390
+                }, {
+                    x: new Date('2019-11-14'),
+                    y: 375
+                }, {
+                    x: new Date('2019-11-15'),
+                    y: 424
+                }, {
+                    x: new Date('2019-11-16'),
+                    y: 272
+                }, {
+                    x: new Date('2019-11-17'),
+                    y: 96
+                }, {
+                    x: new Date('2019-11-19'),
+                    y: 120
+                }]
             }]
+        },
+        options: {
+            title: {
+                text: 'Delhi Pollution Levels in November 2019'
+            },
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Date (in the month of November)'
+                    }
+                }],
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'PM 2.5 Levels'
+                    }
+                }]
+            },
+            plugins: {
+                colorschemes: {
+                    scheme: 'brewer.SetOne3'
+                }
+            }
         }
     }
-});
+);
+
+var cropResidueCycle = new Chart(
+    document.getElementById('crop-residue-cycle').getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: ['Sold', 'Used as fodder', 'Used as cooking fuel', 'Composted', 'Incorporated in soil', 'Thrown away', 'Burnt open field'],
+            datasets: [{
+                label: '% of Use',
+                data: [2, 7, 2, 5, 1, 1, 82],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            plugins: {
+                colorschemes: {
+                    scheme: 'brewer.Paired12'
+                }
+            }
+        }
+    }
+);
+
+var fireBurningIncidents = new Chart(
+    document.getElementById('fire-burning-incidents').getContext('2d'), {
+        type: 'bar',
+        data:{
+            labels: ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'],
+            datasets: [{
+                label: 'Number of fire burning incidents in India',
+                data: [143223, 133267, 172617, 132259, 141962, 126556, 166245, 142181, 165996, 120646],
+                backgroundColor: ['#00909e77', '#00909e77', '#00909e77', '#00909e77', '#00909e77', '#00909e77', '#00909e77', '#00909e77', '#00909e77', '#00909e77'],
+                borderColor: ['#00909e', '#00909e', '#00909e', '#00909e', '#00909e', '#00909e', '#00909e', '#00909e', '#00909e', '#00909e', ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            plugins: {
+                colorschemes: {
+                    scheme: 'brewer.SetThree12'
+                }
+            }
+        }
+    }
+);
